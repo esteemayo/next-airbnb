c64a4c396258f54b75f550fe15ae7ab5609e6019
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-11 13:06:56 +0100

    fix: modify

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,9 @@
-const Container = () => {
-  return <div>Container</div>;
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div>{children}</div>;
 };
 
 export default Container;
