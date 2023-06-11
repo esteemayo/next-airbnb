49cff633a7043f7d63422c155800973cceaf4cfc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-11 12:56:37 +0100

    fix: add Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
