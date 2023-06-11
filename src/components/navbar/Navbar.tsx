7f954dfd6163d133606a04f2c0df6135f815fe01
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-11 13:24:18 +0100

    fix: add Logo component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = () => {
   return (
     <nav className='fixed w-full bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
-        <Container />
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+          </div>
+        </Container>
       </div>
     </nav>
   );
