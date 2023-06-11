84adcb466619059bf2e505e952d1f0c1cf235643
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-11 13:03:34 +0100

    fix: add container component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 import Container from '../Container';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
