20b30e3f045ac740bfc298efd69935a886b375d6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-12 18:14:41 +0100

    fix: add MenuItemProps interface

--- src/components/navbar/MenuItem.tsx
+++ src/components/navbar/MenuItem.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-const MenuItem = () => {
+interface MenuItemProps {
+  onClick(): void;
+  label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
   return <div>MenuItem</div>;
 };
 
