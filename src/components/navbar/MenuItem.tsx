7996756473926f27b71bb24a0310f92fc9e971cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-12 18:16:14 +0100

    fix: add MenuItemProps interface

--- src/components/navbar/MenuItem.tsx
+++ src/components/navbar/MenuItem.tsx
@@ -6,7 +6,14 @@ interface MenuItemProps {
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
-  return <div>MenuItem</div>;
+  return (
+    <div
+      onClick={onClick}
+      className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+    >
+      {label}
+    </div>
+  );
 };
 
 export default MenuItem;
