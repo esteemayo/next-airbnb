6a86678af12541fcc9775af0d90b3a65041c6676
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-13 00:50:48 +0100

    feat: add useCallback react hook

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 interface ModalProps {
   isOpen?: boolean;
   onClose(): void;
