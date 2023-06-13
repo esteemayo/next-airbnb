61c5a94d7489e444b699111e8e0a049c016410c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-13 10:55:22 +0100

    fix: pass props

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -83,6 +83,14 @@ const Modal: React.FC<ModalProps> = ({
               <div className='relative p-6 flex-auto'>{body}</div>
               <div className='flex flex-col gap-2 p-6'>
                 <div className='flex flex-row items-center gap-4 w-full'>
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      outline
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
                   <Button
                     disabled={disabled}
                     label={actionLabel}
