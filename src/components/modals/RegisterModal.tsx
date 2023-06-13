db062c128f31a036f00d3049169b06550b408ee4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-13 12:16:27 +0100

    fix: add packages

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
 const RegisterModal = () => {
   return <div>RegisterModal</div>;
 };
