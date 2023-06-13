3f330f590335f48507522d7b790bf839fdc5207e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 00:41:08 +0100

    fix: add markup

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,7 +7,12 @@ interface HeadingProps {
 }
 
 const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
-  return <div className=''></div>;
+  return (
+    <div className={center ? 'text-center' : 'text-start'}>
+      <div className='text-2xl font-bold'>{title}</div>
+      <div className='font-light text-neutral-500 mt-2'>{subtitle}</div>
+    </div>
+  );
 };
 
 export default Heading;
