37cfaa51ea1ba03aeeae34b09037a421a69ebf9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 00:35:12 +0100

    fix: modify

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -3,7 +3,6 @@
 import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 import Modal from './Modal';
