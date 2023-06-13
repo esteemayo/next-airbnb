c94a74d63c36d1702876e76a793ab66e7cb0aa10
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-13 01:26:05 +0100

    fix: add markup

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -58,7 +58,32 @@ const Modal: React.FC<ModalProps> = ({
     return null;
   }
 
-  return <div>Modal</div>;
+  return (
+    <>
+      <div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
+        <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto'>
+          <div
+            className={`translate duration-300 h-full ${
+              showModal ? 'translate-y-0' : 'translate-y-full'
+            } ${showModal ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <div className='translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+              <div className='flex items-center justify-center p-6 rounded-t relative border-b-[1px]'>
+                <button
+                  onClick={handleClose}
+                  type='button'
+                  className='p-1 border-0 hover:opacity-70 transition absolute left-9'
+                >
+                  <IoMdClose />
+                </button>
+                <div className='text-lg font-semibold'>{title}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
