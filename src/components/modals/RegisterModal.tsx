dce6d3219eb742a5bfd4e978ad1cc9424bd6b5e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 00:02:01 +0100

    fix: add onSubmit function

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -20,6 +20,11 @@ const RegisterModal = () => {
     },
   });
 
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios.post('/api/register', data);
+  };
+
   return <div>RegisterModal</div>;
 };
 
