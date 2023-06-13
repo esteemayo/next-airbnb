6326549b3a14e4a06b010636d216f2867b1d3f32
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 00:14:56 +0100

    fix: add useRegisterModal

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 
 import Avatar from '../Avatar';
+import useRegisterModal from '@/hooks/useRegisterModal';
 import MenuItem from './MenuItem';
 
 const UserMenu = () => {
