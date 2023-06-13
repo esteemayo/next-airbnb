ad9e207b382fd2e556c9137edb5a5a007235f514
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 00:33:03 +0100

    fix: modify

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -38,6 +38,8 @@ const RegisterModal = () => {
     }
   };
 
+  const bodyContent = <div className='flex flex-col gap-4'></div>;
+
   return (
     <Modal
       disabled={isLoading}
@@ -46,6 +48,7 @@ const RegisterModal = () => {
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
