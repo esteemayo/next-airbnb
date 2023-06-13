9f2b82e382076b34d234c4ad86107febec919987
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-13 10:47:37 +0100

    fix: passprops

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -83,7 +83,11 @@ const Modal: React.FC<ModalProps> = ({
               <div className='relative p-6 flex-auto'>{body}</div>
               <div className='flex flex-col gap-2 p-6'>
                 <div className='flex flex-row items-center gap-4 w-full'>
-                  <Button label='My Button' />
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
                 </div>
               </div>
             </div>
