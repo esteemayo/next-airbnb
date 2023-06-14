5d6cbbb0a577b32ad70b4d6a9b67a61eaace1b6b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 01:18:10 +0100

    fix: add markup

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -48,7 +48,9 @@ const Input: React.FC<InputProps> = ({
         htmlFor={id}
         className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] ${
           formatPrice ? 'left-9' : 'left-4'
-        } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
+        } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 ${
+          errors[id] ? 'text-rose-500' : 'text-zinc-400'
+        }`}
       >
         {label}
       </label>
