a05a7db9cc3041d1107261075e4ac8c812e191a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 01:57:57 +0100

    feat: add httpService file

---
+++ src/services/httpService.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+import logger from './logService';
+
+const authFetch = axios.create({
+  baseURL: '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+authFetch.interceptors.request.use(
+  (req) => {
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error('An unexpected error occurred');
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: authFetch.get,
+  post: authFetch.post,
+  patch: authFetch.patch,
+  delete: authFetch.delete,
+};
+
+export default http;
