7ab650aa58f0778fc1dd4732c408a4c2c17f136c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 22:53:50 +0100

    feat: handle register user logic

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,37 @@
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+import prisma from '@/libs/prismadb';
+
+interface UserInputs {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const POST = async (request: Request) => {
+  const body = await request.json();
+  const { name, email, password }: UserInputs = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        hashedPassword,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(user, {
+        status: 201,
+      });
+    }
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
