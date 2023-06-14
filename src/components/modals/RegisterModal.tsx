19db48b122debe8e2f8f25b42c7cf94451cdca1f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-14 01:45:53 +0100

    fix: import react-hot-toast

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -5,6 +5,7 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { useCallback, useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 import Modal from './Modal';
 import useRegisterModal from '@/hooks/useRegisterModal';
