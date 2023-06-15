817c1d687ac1ff93271354e06399beaaab661373
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-15 02:04:20 +0100

    fix: modify

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
