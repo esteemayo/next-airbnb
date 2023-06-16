f9f7c677972f67b29871e507ef5ff7503c462c3c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 19:14:05 +0100

    fix: modify

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import GithubProvider from 'next-auth/providers/github';
 import bcrypt from 'bcryptjs';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 import User from '@/models/User';
