d53bf9657d6e17600920b804c4cfef49021b3b97
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 21:39:37 +0100

    feat: setup a reservation model schema

---
+++ src/models/Reservation.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const reservationSchema = new Schema(
+  {
+    startDate: {
+      type: Date,
+      required: [true, 'A reservation must have a start date'],
+    },
+    endDate: {
+      type: Date,
+      required: [true, 'A reservation must have an end date'],
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, 'A reservation must have a total price'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    listing: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Listing',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Reservation =
+  mongoose.models.Reservation ||
+  mongoose.model('Reservation', reservationSchema);
+
+export default Reservation;
