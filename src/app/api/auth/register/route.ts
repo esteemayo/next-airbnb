801b8ca4eda6d13119147601ffe973e83e23574f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 21:47:49 +0100

    fix: modify

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -13,6 +13,8 @@ export const POST = async (request: Request) => {
   const body = await request.json();
   const { name, email, password }: UserInputs = body;
 
+  await connectDB();
+
   const newUser = {
     name,
     email,
