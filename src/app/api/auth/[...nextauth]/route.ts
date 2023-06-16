376d900046e21c648ae2946902c68b528d4c07e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 19:12:49 +0100

    fix: modify

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import GithubProvider from 'next-auth/providers/github';
 import bcrypt from 'bcryptjs';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
