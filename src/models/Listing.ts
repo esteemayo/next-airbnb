d04f4da2205fafc5afe15a8d9d2ea1d6ddacd2cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 21:37:55 +0100

    fix: modify

--- src/models/Listing.ts
+++ src/models/Listing.ts
@@ -15,53 +15,58 @@ interface ListingModel {
   reservations: object;
 }
 
-const listingSchema = new Schema<ListingModel>({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, 'A list item must have a title'],
+const listingSchema = new Schema<ListingModel>(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'A list item must have a title'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A list item must have a description'],
+    },
+    imageSrc: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: [true, 'A list item must belong to a category'],
+    },
+    roomCount: {
+      type: Number,
+      required: [true, 'A list item must have a room count'],
+    },
+    bathroomCount: {
+      type: Number,
+      required: [true, 'A list item must have a bathroom count'],
+    },
+    guestCount: {
+      type: Number,
+      required: [true, 'A list item must have a guest count'],
+    },
+    locationValue: {
+      type: String,
+      required: [true, 'A list item must have a location'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A list item must have a price'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    reservations: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Reservation',
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A list item must have a description'],
-  },
-  imageSrc: {
-    type: String,
-  },
-  category: {
-    type: String,
-    required: [true, 'A list item must belong to a category'],
-  },
-  roomCount: {
-    type: Number,
-    required: [true, 'A list item must have a room count'],
-  },
-  bathroomCount: {
-    type: Number,
-    required: [true, 'A list item must have a bathroom count'],
-  },
-  guestCount: {
-    type: Number,
-    required: [true, 'A list item must have a guest count'],
-  },
-  locationValue: {
-    type: String,
-    required: [true, 'A list item must have a location'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A list item must have a price'],
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-  },
-  reservations: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Reservation',
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Listing =
   mongoose.models.Listing || mongoose.model('Listing', listingSchema);
