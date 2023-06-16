4c23d74590783b72e2b23c1a48e947d8a4f76d21
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-16 21:17:10 +0100

    feat: setup an account model schema

---
+++ src/models/Account.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const accountSchema = new Schema(
+  {
+    type: {
+      type: String,
+      required: [true, 'An account must have a type'],
+    },
+    provider: {
+      type: String,
+      required: [true, 'An account must have a provider'],
+    },
+    providerAccountId: {
+      type: String,
+      required: [true, 'An account must have a provider account id'],
+    },
+    refresh_token: {
+      type: String,
+    },
+    access_token: {
+      type: String,
+    },
+    expires_at: {
+      type: String,
+    },
+    token_type: {
+      type: String,
+    },
+    scope: {
+      type: String,
+    },
+    id_token: {
+      type: String,
+    },
+    session_state: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account =
+  mongoose.models.Account || mongoose.model('Account', accountSchema);
+
+export default Account;
