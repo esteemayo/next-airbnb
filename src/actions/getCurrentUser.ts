233227b041f99cf7d38229e1edcb37a334734820
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-17 02:07:27 +0100

    feat: create a file to get current user

---
+++ src/actions/getCurrentUser.ts
@@ -0,0 +1,30 @@
+import { getServerSession } from 'next-auth/next';
+
+import User from '@/models/User';
+import { authOptions } from '@/app/pages/[...nextauth]';
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await User.findOne({
+      email: session.user.email as string,
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (err: any) {
+    return null;
+  }
+}
