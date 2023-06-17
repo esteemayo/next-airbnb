0ad8bc3c73952b8053b792a7ef18e8beab9dc8da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-17 04:02:48 +0100

    feat: create a AuthProvider file to access session data

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,11 @@
+import { SessionProvider } from 'next-auth/react';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
