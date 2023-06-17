68bf0873c466e80a6dc99b7f6ba4ab37cb2c5fda
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-17 01:52:22 +0100

    fix: fix a bug

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -17,7 +17,7 @@ import Button from '../Button';
 import Input from '../inputs/Input';
 
 const LoginModal = () => {
-  const router = useRouter;
+  const router = useRouter();
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
 
