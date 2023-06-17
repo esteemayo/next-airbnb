d6aef2babb23bd8c2fecd7dc3c0df43cf31bc9c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 00:58:23 +0100

    fix: modify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com']
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ],
   }
 }
 
