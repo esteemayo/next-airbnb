00f51a7b9cbe9605b33170951f5d4208ff25e3b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 13:29:35 +0100

    fix: add useRouter next/navigation hook

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 const Logo = () => {
+  const router = useRouter();
+
   return (
     <Image
       src='/images/logo.png'
