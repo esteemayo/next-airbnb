e8f6cfa0d5088384c34c0ada8726364d9f4bcf50
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 02:08:33 +0100

    fix: modify

--- src/components/navbar/Categories.tsx
+++ src/components/navbar/Categories.tsx
@@ -9,8 +9,8 @@ const Categories = () => {
     <Container>
       <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
         {categories.map((item) => {
-          const { icon, label, description } = item;
-          return <CategoryBox key={label} label={label} description={description} icon={icon} />;
+          const { icon, label } = item;
+          return <CategoryBox key={label} label={label} icon={icon} />;
         })}
       </div>
     </Container>
