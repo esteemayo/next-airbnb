3eb42c732a219bb836a4887ab65f8e91b9f9f763
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 13:27:33 +0100

    fix: modify

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -4,15 +4,13 @@ import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <div>
-      <Image
-        src='/images/logo.png'
-        width={100}
-        height={100}
-        alt='logo'
-        className='hidden md:block cursor-pointer'
-      />
-    </div>
+    <Image
+      src='/images/logo.png'
+      width={100}
+      height={100}
+      alt='logo'
+      className='hidden md:block cursor-pointer'
+    />
   );
 };
 
