59e74489094a1b116035f2430482dcd2452e6ee3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 02:39:52 +0100

    fix: add handleClick function

--- src/components/CategoryBox.tsx
+++ src/components/CategoryBox.tsx
@@ -48,6 +48,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
 
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
         selected ? 'border-b-neutral-800' : 'border-transparent'
       } ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}
