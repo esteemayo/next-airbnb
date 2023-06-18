b6a830d027d009235e2efd95985de3e4162747ab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 02:04:53 +0100

    fix: add markup

--- src/components/navbar/Categories.tsx
+++ src/components/navbar/Categories.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import Container from '../Container';
+import CategoryBox from '../CategoryBox';
+import { categories } from '@/data';
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <Container>
+      <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+        {categories.map((item) => {
+          const { icon, label, description } = item;
+          return <CategoryBox key={label} label={label} description={description} icon={icon} />;
+        })}
+      </div>
+    </Container>
+  );
 };
 
 export default Categories;
