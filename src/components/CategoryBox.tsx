4d940e1d2a857f19a5a4ee11ac37905380cca528
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 02:19:28 +0100

    feat: add useSearchParams next/navigation hook

--- src/components/CategoryBox.tsx
+++ src/components/CategoryBox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { IconType } from 'react-icons';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface CategoryBoxProps {
   icon: IconType;
