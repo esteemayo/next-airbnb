b8fb59a064be93d8357101e7800837acf8dcf1cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-18 01:10:35 +0100

    fix: modify

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -3,8 +3,7 @@
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import { useCallback, useState } from 'react';
-import { toast } from 'react-hot-toast';
+import {  useState } from 'react';
 import { signIn } from 'next-auth/react';
 
 import Modal from './Modal';
