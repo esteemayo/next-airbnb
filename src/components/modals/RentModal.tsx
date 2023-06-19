22098dfb8c94a9ca73f7166566fe8b2f01867974
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 01:33:33 +0100

    fix: create STEPS enumeration

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -3,6 +3,15 @@
 import Modal from './Modal';
 import useRentModal from '@/hooks/useRentModal';
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
+
 const RentModal = () => {
   const rentModal = useRentModal();
 
