0df01cbf592d0fceeefd4cf8e8ba9ab356d62dcd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 05:05:53 +0100

    fix: modify

--- src/components/inputs/CountrySelect.tsx
+++ src/components/inputs/CountrySelect.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import Select from 'react-select'
+import Select from 'react-select';
 
-const CountrySelect = () => {
-  return (
-    <div>CountrySelect</div>
-  )
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+};
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange(value: CountrySelectValue): void;
 }
 
-export default CountrySelect
+const CountrySelect:React.FC<CountrySelectProps> = ({value,onChange}) => {
+  return <div>CountrySelect</div>;
+};
+
+export default CountrySelect;
