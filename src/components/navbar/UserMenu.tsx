7b8b96177b001f55c5bb6810fa5462e32f01aa72
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 01:03:13 +0100

    fix: add onRent useCallback function

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -31,7 +31,7 @@ const UserMenu = () => {
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
         <div
-          onClick={() => {}}
+          onClick={onRent}
           className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
         >
           Airbnb your home
