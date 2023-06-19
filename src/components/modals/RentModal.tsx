a1a54b90a5e3d61c58eadf40faa3125e6fcf6542
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 16:32:49 +0100

    fix: add map

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -9,7 +9,7 @@ import Heading from '../Heading';
 import { categories } from '@/data';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
-import Map from '../Map';
+import dynamic from 'next/dynamic';
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,6 +46,14 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -114,7 +122,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
-        <Map />
+        <Map center={location?.latlng} />
       </div>
     );
   }
