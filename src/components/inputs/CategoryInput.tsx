9ce403add055d6e3de94285ab13f9d13ec0a10a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 02:16:54 +0100

    fix: add IconType react-icons hook

--- src/components/inputs/CategoryInput.tsx
+++ src/components/inputs/CategoryInput.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import { IconType } from 'react-icons';
+
+interface CategoryInputProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+  onClick(value: string): void;
+}
+
 const CategoryInput = () => {
   return (
     <div>CategoryInput</div>
