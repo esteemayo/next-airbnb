fada1fbaf25460384a124c5a655615ef1bf32c87
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-19 02:23:59 +0100

    fix: add markup

--- src/components/inputs/CategoryInput.tsx
+++ src/components/inputs/CategoryInput.tsx
@@ -15,7 +15,16 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
   selected,
   onClick,
 }) => {
-  return <div>CategoryInput</div>;
+  return (
+    <div
+      className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${
+        selected ? 'border-black' : 'border-neutral-200'
+      }`}
+    >
+      <Icon size={30} />
+      <div className='font-semibold'>{label}</div>
+    </div>
+  );
 };
 
 export default CategoryInput;
