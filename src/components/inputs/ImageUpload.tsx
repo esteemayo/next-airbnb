fe6254eca652a7e658a4f9e6f266d0c93fcbc338
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 17:50:56 +0100

    fix: add markup

--- src/components/inputs/ImageUpload.tsx
+++ src/components/inputs/ImageUpload.tsx
@@ -1,9 +1,45 @@
 'use client';
 
-const ImageUpload = () => {
-  return (
-    <div>ImageUpload</div>
-  )
+import { CldUploadWidget } from 'next-cloudinary';
+import Image from 'next/image';
+import { TbPhotoPlus } from 'react-icons/tb';
+import { useCallback } from 'react';
+
+declare global {
+  var cloudinary: any;
 }
 
-export default ImageUpload
+interface ImageUploadProps {
+  onChange(value: string): void;
+  value: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset=''
+      options={{ maxFiles: 1 }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className='relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col items-center justify-center gap-4 text-neutral-600'
+          >
+            <TbPhotoPlus size={50} />
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
