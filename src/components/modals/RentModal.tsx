788cc112412ec06ab8aff62593e218a36d0823e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 17:15:41 +0100

    fix: add markup

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -162,6 +162,17 @@ const RentModal = () => {
     );
   }
 
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
