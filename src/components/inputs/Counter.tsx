5f8278ae7203948bf2379865987f8d90045bf066
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 14:59:01 +0100

    fix: add CounterProps interface

--- src/components/inputs/Counter.tsx
+++ src/components/inputs/Counter.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+interface CounterProps {
+  title: string;
+  subtitle: string;
+  value: number;
+  onChange(value: number): void;
+}
+
 const Counter = () => {
   return <div>Counter</div>;
 };
