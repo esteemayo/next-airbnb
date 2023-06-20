a293172ed12ca005979cce6b2bac11252767954f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 15:05:48 +0100

    feat: add onAdd useCallback function

--- src/components/inputs/Counter.tsx
+++ src/components/inputs/Counter.tsx
@@ -9,7 +9,15 @@ interface CounterProps {
   onChange(value: number): void;
 }
 
-const Counter:React.FC<CounterProps> = ({ title, subtitle, value, onChange }) => {
+const Counter: React.FC<CounterProps> = ({
+  title,
+  subtitle,
+  value,
+  onChange,
+}) => {
+  const onAdd = useCallback(() => {
+    onChange(value + 1);
+  }, [onChange, value]);
   return <div>Counter</div>;
 };
 
