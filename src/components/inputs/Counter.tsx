23b8aa6bb056032f71f9760ee7c1414198788926
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 15:17:23 +0100

    fix: add markup

--- src/components/inputs/Counter.tsx
+++ src/components/inputs/Counter.tsx
@@ -34,6 +34,14 @@ const Counter: React.FC<CounterProps> = ({
         <div className='font-medium'>{title}</div>
         <div className='font-light text-gray-600'>{subtitle}</div>
       </div>
+      <div className='flex flex-row items-center gap-4'>
+        <div
+          onClick={onReduce}
+          className='w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
+        >
+          <AiOutlineMinus />
+        </div>
+      </div>
     </div>
   );
 };
