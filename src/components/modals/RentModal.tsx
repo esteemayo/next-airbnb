07191435ebe2aeb2cff8d9a33649782518f7c97e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-20 18:54:11 +0100

    fix: add react-hot-toast module

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -4,6 +4,7 @@ import { useMemo, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
 
 import CategoryInput from '../inputs/CategoryInput';
 import Counter from '../inputs/Counter';
