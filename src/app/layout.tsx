9adc9041fd6b451b201b0165ba3a09f88b718170
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-21 01:53:41 +0100

    fix: modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
             <LoginModal />
             <RegisterModal />
           </ClientOnly>
-          {children}
+          <div className='pb-20 pt-28'>{children}</div>
         </AuthProvider>
       </body>
     </html>
