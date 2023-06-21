503d96e17cbf630e22a5daa4680c3ace0183755f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-21 01:59:07 +0100

    fix: add markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+'use client';
+
+import Container from '@/components/Container';
+import ClientOnly from '@/components/ClientOnly';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 };
 
 export default Home;
