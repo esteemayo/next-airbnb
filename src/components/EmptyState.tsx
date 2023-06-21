15b378ea213eff522b91f597da88ba0a523165f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-21 02:32:57 +0100

    fix: add EmptyStateProps interface

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -4,7 +4,11 @@ interface EmptyStateProps {
   showReset?: boolean;
 }
 
-const EmptyState = () => {
+const EmptyState:React.FC<EmptyStateProps> = ({ 
+  title = 'No exact matches',
+  subtitle = 'Try changing or removing some of your filters',
+  showReset,
+}) => {
   return <div>EmptyState</div>;
 };
 
