42ea1cbff07a78ebccb074645684ebc9ae3c7f6f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-21 03:53:22 +0100

    fix: modify

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -1,12 +1,15 @@
 import prisma from '@/libs/prismadb';
+import Listing from '@/models/Listing';
 
 export default async function getListings() {
   try {
-    const listings = await prisma.listing.findMany({
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
+    const listings = await Listing.find().sort('-createdAt');
+
+    // const listings = await prisma.listing.findMany({
+    //   orderBy: {
+    //     createdAt: 'desc',
+    //   },
+    // });
 
     return listings;
   } catch (err: any) {
