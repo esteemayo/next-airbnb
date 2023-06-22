1ee64bc48a91d501655a69bfba2410c7ef7029bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-22 14:47:46 +0100

    fix: add markup

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -79,6 +79,18 @@ const ListingCard: React.FC<ListingCardProps> = ({
             <HeartButton listingId={data._id} />
           </div>
         </div>
+        <div className='font-semibold text-lg'>
+          {location?.region}, {location?.label}
+        </div>
+        <div className='font-light text-neutral-500'>
+          {reservationDate || data.category}
+        </div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'>$ {price}</div>
+          {!reservation && (
+            <div className='font-light'>night</div>
+          )}
+        </div>
       </div>
     </div>
   );
