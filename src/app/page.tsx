3cb60a3b52ec532a1c362b55751163578b54e263
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-22 01:33:29 +0100

    fix: rename component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Container from '@/components/Container';
 import ClientOnly from '@/components/ClientOnly';
 import EmptyState from '@/components/EmptyState';
 import { getListings } from '@/services/listingService';
-import Listings from '@/components/Listings';
+import ListingCard from '@/components/ListingCard';
 
 const Home = () => {
   const [listings, setListings] = useState([]);
@@ -38,7 +38,7 @@ const Home = () => {
       <Container>
         <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
           {listings.map((listing) => {
-            return <Listings key={listing._id} listing={listing} />;
+            return <ListingCard key={listing._id} listing={listing} />;
           })}
         </div>
       </Container>
