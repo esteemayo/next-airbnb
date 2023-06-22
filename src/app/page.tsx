4a28e5b55fed4ea31d93b7b320a7c3ecae714250
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-22 02:00:19 +0100

    fix: modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ const Home = () => {
       <Container>
         <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
           {listings.map((listing: any) => {
-            return <ListingCard key={listing._id} listing={listing} />;
+            return <ListingCard key={listing._id} data={listing} />;
           })}
         </div>
       </Container>
