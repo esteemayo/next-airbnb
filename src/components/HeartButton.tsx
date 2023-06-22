7f4620ed1df1a91bff3ea6675906b6676dd13ab4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-22 14:39:40 +0100

    fix: add markup

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -1,8 +1,30 @@
 'use client';
 
-const HeartButton = () => {
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+interface HeartButtonProps {
+  listingId: string;
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({ listingId }) => {
+  const hasFavorited = false;
+
+  const toggleFavorite = () => {};
+
   return (
-    <div>HeartButton</div>
+    <div
+      onClick={toggleFavorite}
+      className='relative hover:opacity-80 transition cursor-pointer'
+    >
+      <AiOutlineHeart
+        size={28}
+        className='fill-white absolute -top-[2px] -right-[2px]'
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFovorited ? 'fill-rose-500' : 'fill-neutral-500/70'}
+      />
+    </div>
   );
 };
 
