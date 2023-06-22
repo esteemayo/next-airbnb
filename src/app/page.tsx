180ce18e053a6398ca37350b8a73c5c9e547e0d8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-22 01:23:09 +0100

    fix: modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const Home = () => {
   return (
     <ClientOnly>
       <Container>
-        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
           <div>My future listings</div>
         </div>
       </Container>
