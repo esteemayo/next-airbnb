b92b1fad0de4445e0ab6b6adb46c4507af95f51b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 15:13:17 +0100

    feat: create useFavorite custom hook

---
+++ src/hooks/useFavorite.ts
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
+import { toast } from 'react-hot-toast';
+
+import useLoginModal from './useLoginModal';
+import { createFavorite, deleteFavorite } from '@/services/favoriteService';
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: object | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+    return list.includes[listingId];
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => deleteFavorite(listingId);
+        } else {
+          request = () => createFavorite(listingId);
+        }
+
+        await request();
+        router.refresh();
+        toast.success('Success');
+      } catch (err: any) {
+        console.log(err.message);
+        toast.error('Something went wrong.');
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
