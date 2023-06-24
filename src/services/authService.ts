17b32672c0ad6817d6cc7c618ef75fef0cc52772
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 01:20:24 +0100

    fix: modify

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,11 +2,5 @@ import http from './httpService';
 
 const apiEndpoint = '/auth/register';
 
-interface RegisterInput {
-  name: string;
-  email: string;
-  password: string;
-}
-
 export const registerUser = (credentials: any) =>
   http.post(apiEndpoint, credentials);
