9ffa2dcdaf2f18b51dc30456a70b362ba7948cca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 23:43:22 +0100

    fix: modify

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { toast } from 'react-hot-toast';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import ClientOnly from '@/components/ClientOnly';
 import { getListing } from '@/services/listingService';
@@ -12,22 +12,21 @@ interface IParams {
   listingId?: string;
 }
 
-const ListingPage = ({ params }: { params: IParams }) => {
-  const { listingId } = params;
+const ListingPage = ({ params: { listingId } }: { params: IParams }) => {
   const [listing, setListing] = useState({});
 
-  const fetchListing = async () => {
+  const fetchListing = useCallback(async () => {
     try {
       const { data } = await getListing(listingId);
       setListing(data);
     } catch (err: any) {
       toast.error(err.response.data.message);
     }
-  };
+  }, [listingId]);
 
   useEffect(() => {
     listingId && fetchListing();
-  }, [listingId]);
+  }, [listingId, fetchListing]);
 
   if (!listing) {
     return (
