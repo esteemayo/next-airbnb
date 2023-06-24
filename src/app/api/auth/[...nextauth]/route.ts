0fe5a3941c679ed934e771ccffce0fc4b292b096
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 01:09:21 +0100

    fix: modify

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -32,7 +32,6 @@ export const options: NextAuthOptions = {
           placeholder: '********',
         },
       },
-      // @ts-ignore
       async authorize(credentials) {
         await connectDB();
         const { email, password } = credentials as {
