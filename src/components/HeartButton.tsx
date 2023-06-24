4ef9a09eed926e5f32153acfae55a1d6479c05df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 15:35:34 +0100

    fix: handle favourite listing functionality

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '@/hooks/useFavorite';
 
 interface HeartButtonProps {
   listingId: string;
+  currentUser?: object | null;
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({ listingId }) => {
-  const hasFavorited = false;
-
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
 
   return (
     <div
