aba175027a9e09307e6aeca58d2618c39ba3d09d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 16:26:32 +0100

    fix: add single listing api url endpoint

--- src/services/listingService.ts
+++ src/services/listingService.ts
@@ -4,4 +4,7 @@ const apiEndpoint = '/listings';
 
 export const getListings = () => http.get(apiEndpoint);
 
+export const getListing = (listingId: string) =>
+  http.get(`${apiEndpoint}/${listingId}`);
+
 export const createListing = (listing: any) => http.post(apiEndpoint, listing);
