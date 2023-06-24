3c609b13d44da9c447d641b02384b9c0592689e5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-25 00:57:04 +0100

    fix: add markup

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -1,8 +1,40 @@
 'use client';
 
-const ListingClient = () => {
+import { useMemo } from 'react';
+import { useSession } from 'next-auth/react';
+
+import Container from '@/components/Container';
+import { categories } from '@/data';
+import ListingHead from '@/components/listings/ListingHead';
+
+interface ListingClientProps {
+  reservations?: Array;
+  listing: object;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+  reservations,
+  listing,
+}) => {
+  const session = useSession();
+
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+
   return (
-    <div>ListingClient</div>
+    <Container>
+      <div className='max-w-screen-lg max-auto'>
+        <div className='flex flex-col gap-6'>
+          <ListingHead
+            id={listing._id}
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+          />
+        </div>
+      </div>
+    </Container>
   );
 };
 
