9248693f6c4a9820d36be10c60f35e642eb722a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 23:18:12 +0100

    fix: fetch listing

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -1,8 +1,31 @@
 'use client';
 
-import { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
 
-const ListingPage = () => {
+import { getListing } from '@/services/listingService';
+
+interface IParams {
+  listingId?: string;
+}
+
+const ListingPage = ({ params }: { params: IParams }) => {
+  const [listing, setListing] = useState({});
+  const { listingId } = params;
+
+  const fetchListing = async () => {
+    try {
+      const { data } = await getListing(listingId);
+      setListing(data);
+    } catch (err: any) {
+      toast.error(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    listingId && fetchListing();
+  }, [listingId]);
+
   return <div>ListingPage</div>;
 };
 
