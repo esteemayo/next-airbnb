779b588de5deee00f3fd7b3db17372ea55a16144
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 14:45:42 +0100

    feat: add create favorite api endpoint

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -2,5 +2,8 @@ import http from './httpService';
 
 const apiEndpoint = '/favorites';
 
+export const createFavorite = (listingId: string) =>
+  http.post(`${apiEndpoint}/${listingId}`);
+
 export const deleteFavorite = (listingId: string) =>
   http.delete(`${apiEndpoint}/${listingId}`);
