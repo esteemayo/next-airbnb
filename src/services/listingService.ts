3ae266052f668bac6a23b3de664cb3e1481bd6a8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-24 16:28:10 +0100

    fix: refactor code

--- src/services/listingService.ts
+++ src/services/listingService.ts
@@ -2,9 +2,11 @@ import http from './httpService';
 
 const apiEndpoint = '/listings';
 
+const listingUrl = (listingId) => `${apiEndpoint}/${listingId}`;
+
 export const getListings = () => http.get(apiEndpoint);
 
 export const getListing = (listingId: string) =>
-  http.get(`${apiEndpoint}/${listingId}`);
+  http.get(listingUrl(listingId));
 
 export const createListing = (listing: any) => http.post(apiEndpoint, listing);
