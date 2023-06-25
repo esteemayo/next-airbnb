4aaed4a49215e59148c526866c22a8a73a21b4d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-25 01:16:18 +0100

    fix: add ListingInfo component

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -5,7 +5,9 @@ import { useSession } from 'next-auth/react';
 
 import Container from '@/components/Container';
 import { categories } from '@/data';
+
 import ListingHead from '@/components/listings/ListingHead';
+import ListingInfo from '@/components/listings/ListingInfo';
 
 interface ListingClientProps {
   reservations?: Array;
@@ -32,6 +34,17 @@ const ListingClient: React.FC<ListingClientProps> = ({
             imageSrc={listing.imageSrc}
             locationValue={listing.locationValue}
           />
+          <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+            <ListingInfo
+              user={listing.user}
+              category={listing.category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
