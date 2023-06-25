d88ea6392c2c6ee6666fe73b529a2aba3f917053
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-25 23:35:18 +0100

    fix: add markup

--- src/components/listings/ListingCategory.tsx
+++ src/components/listings/ListingCategory.tsx
@@ -10,8 +10,10 @@ interface ListingCategoryProps {
 
 const ListingCategory: React.FC<ListingCategoryProps> = ({ icon: Icon, label, description }) => {
   return (
-    <div>
-      ListingCategory
+    <div className='flex flex-col gap-6'>
+      <div className='flex flex-row items-center gap-4'>
+        <Icon size={40} classname='text-neutral-600' />
+      </div>
     </div>
   );
 };
