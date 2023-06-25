1c037429656f1364d8ad8847d45934cee3d634a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-25 23:10:24 +0100

    fix: add markup

--- src/components/listings/ListingInfo.tsx
+++ src/components/listings/ListingInfo.tsx
@@ -33,7 +33,11 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   const coordinates = getByValue(locationValue)?.latlng;
   return (
     <div className='col-span-4 flex flex-col gap-8'>
-      <div className='flex flex-col gap-2'></div>
+      <div className='flex flex-col gap-2'>
+        <div className='text-xl font-semibold flex flex-row items-center gap-2'>
+
+        </div>
+      </div>
     </div>
   );
 };
