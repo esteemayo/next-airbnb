fdcb60c4a52dc09f9b8d7d43d4f1ccfb316dedca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-26 15:36:52 +0100

    fix: add markup

--- src/components/listings/ListingReservation.tsx
+++ src/components/listings/ListingReservation.tsx
@@ -22,8 +22,11 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   disabledDates,
 }) => {
   return (
-    <div className=''>
-      Listing Reservation
+    <div className='bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden'>
+      <div className='flex flex-row items-center gap-1 p-4'>
+        <div className='text-2xl font-semibold'>$ {price}</div>
+        <div className='font-light text-neutral-500'>night</div>
+      </div>
     </div>
   );
 };
