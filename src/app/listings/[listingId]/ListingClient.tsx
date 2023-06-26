a7c18c6af5bc89b86f907e5da18853ec1eb1525d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-26 01:42:50 +0100

    fix: add useState react hook

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { eachDayOfInterval } from 'date-fns';
