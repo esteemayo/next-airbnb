6d48f28dd231800f3ffcdfc58f92851c08bbe1aa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-26 15:23:10 +0100

    fix: add interface ListingReservationProps

--- src/components/listings/ListingReservation.tsx
+++ src/components/listings/ListingReservation.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+interface ListingReservationProps {
+  price: number,
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate(value: Range): void;
+  onSubmit(): void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
 const ListingReservation = () => {
   return (
     <div className=''>
