d894667eb41d77039d37ff19bff5c8f9d6d94c68
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-26 15:52:40 +0100

    fix: add CalendarProps interface

--- src/components/inputs/Calendar.tsx
+++ src/components/inputs/Calendar.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { Range, RangeKeyDict } from 'react-date-range';
+
+interface CalendarProps {
+  value: Range,
+  onChange(value: RangeKeyDict): void;
+  disabledDates?: Date[];
+}
+
 const Calendar = () => {
   return (
     <div>
