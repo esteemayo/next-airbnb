c6c79131d64a0047ffe32fcb34081ed66c9aac5d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 02:48:58 +0100

    fix: add create reservation api logic

---
+++ src/app/api/reservations/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server';
+
+import connectDB from '@/utils/db';
+import { getLoggedInUser } from '@/actions/getLoggedInUser';
+import Listing from '@/models/Listing';
+import Reservation from '@/models/Reservation';
+
+export const POST = async (request: Request) => {
+  const currentUser = await getLoggedInUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+
+  const { startDate, endDate, totalPrice, listing } = body;
+
+  if (!startDate || !endDate || !totalPrice || !listing) {
+    return NextResponse.error();
+  }
+
+  const newReservation = {
+    startDate,
+    endDate,
+    totalPrice,
+    listing,
+  };
+
+  try {
+    await connectDB();
+
+    const reservationPromise = Reservation.create({ ...newReservation });
+    const newListingPromise = Listing.findByIdAndUpdate(listing, {
+      $push: { reservations: newReservation },
+    });
+
+    const [reservation, newListing] = await Promise.all([
+      reservationPromise,
+      newListingPromise,
+    ]);
+
+    return NextResponse.json(
+      { reservation, newListing },
+      {
+        status: 201,
+      }
+    );
+  } catch (err: any) {
+    return NextResponse.json(err.message);
+  }
+};
