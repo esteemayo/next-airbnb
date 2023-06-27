48fe05f8679cbb24b6fe409c02d1dc90279373da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 19:02:51 +0100

    fix: add fetchReservations useCallback handler function

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,14 +1,24 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 
 import ClientOnly from '@/components/ClientOnly';
 import EmptyState from '@/components/EmptyState';
+import { getReservations } from '@/services/reservationService';
 
 const TripsPage = () => {
   const session = useSession();
   const [reservations, setReservations] = useState([]);
+
+  const fetchReservations = useCallback(async () => {
+    try {
+      const { data } = await getReservations({ user: session?.data?.user.email });
+      setReservation(data);
+    } catch (err: any) {
+      console.log(err);
+    }
+  }, []);
 
   if (session.status === 'unauthenticated') {
     return (
