c10f8a246d11a3e39120c0ef5da78f946cff0161
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 18:55:14 +0100

    fix: modify

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
+
+import ClientOnly from '@/components/ClientOnly';
+import EmptyState from '@/components/EmptyState';
+
 const TripsPage = () => {
+  const session = useSession();
+
   return (
     <div>
       TripsPage
