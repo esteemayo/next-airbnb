265e4a0a6d5e91016a36a8bebef84fea151b01e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 18:40:41 +0100

    fix: add get reservations api endpoint

--- src/services/reservationService.ts
+++ src/services/reservationService.ts
@@ -2,5 +2,7 @@ import http from './httpService';
 
 const apiEndpoint = '/reservations';
 
+export const getReservations = (query: any) => http.get(apiEndpoint, query);
+
 export const createReservation = (reservation: any) =>
   http.post(apiEndpoint, reservation);
