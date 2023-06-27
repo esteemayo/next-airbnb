e6879b8018d260dc78a968d51c5f70615a266bee
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 18:59:43 +0100

    fix: add useCallback react hook

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useSession } from 'next-auth/react';
 
 import ClientOnly from '@/components/ClientOnly';
