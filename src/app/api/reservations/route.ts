0439477b3e921bdf997d44477ab71090a22f450d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-27 18:35:23 +0100

    fix: modify

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -5,6 +5,52 @@ import { getLoggedInUser } from '@/actions/getLoggedInUser';
 import Listing from '@/models/Listing';
 import Reservation from '@/models/Reservation';
 
+interface IParams {
+  listing?: string;
+  user?: string;
+  authorId?: string;
+}
+
+export const GET = async (
+  request: Request,
+  { params }: { params: IParams }
+) => {
+  try {
+    await connectDB();
+
+    const { listing, user, authorId } = params;
+
+    const query: any = {};
+
+    if (listing) {
+      query.listing = listing;
+    }
+
+    if (user) {
+      query.user = user;
+    }
+
+    if (authorId) {
+      query.authorId = { user: authorId };
+    }
+
+    const reservations = await Reservation.find(query);
+
+    const safeReservation = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toDateString(),
+      endDate: reservation.endDate.toISOString(),
+    }));
+
+    return safeReservation;
+  } catch (err: any) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
+
 export const POST = async (request: Request) => {
   const currentUser = await getLoggedInUser();
 
