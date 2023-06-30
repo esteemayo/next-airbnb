a9532dfbf7d89ebf42652fc36b7fe3cfe4998d66
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-30 17:30:26 +0100

    fix: fix a bug

--- src/app/api/reservations/[reservationId]/route.ts
+++ src/app/api/reservations/[reservationId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 import connectDB from '@/utils/db';
-import getLoggedInUser from '@/actions/getLoggedInUser';
+import { getLoggedInUser } from '@/actions/getLoggedInUser';
 import Reservation from '@/models/Reservation';
 
 interface IParams {
