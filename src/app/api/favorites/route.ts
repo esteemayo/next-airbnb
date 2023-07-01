2c98a33015a9bc41a4a9368aafb83583b21aeecc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-01 19:48:01 +0100

    feat: add get current user` favorite listings api endpoint logic

---
+++ src/app/api/favorites/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+
+import connectDB from '@/utils/db';
+import Listing from '@/models/Listing';
+import User from '@/models/User';
+import { getLoggedInUser } from '@/actions/getLoggedInUser';
+
+export const GET = async (request: Request) => {
+  try {
+    await connectDB();
+
+    const currentUser = await getLoggedInUser();
+
+    if (!currentUser) {
+      return [];
+    }
+
+    const user = await User.findOne({ email: currentUser.email });
+    const favListings = user.favoriteIds;
+    const favorites = await Promise.all(
+      favListings.map((favoriteId: string) => {
+        return Listing.findById(favoriteId);
+      })
+    );
+
+    return NextResponse.json(
+      { user, favorites },
+      {
+        status: 200,
+      }
+    );
+  } catch (err: any) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
