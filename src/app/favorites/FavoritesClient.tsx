d055ffba705e876566053cd7fc240a6be15d6363
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-01 20:24:09 +0100

    fix: add markup

--- src/app/favorites/FavoritesClient.tsx
+++ src/app/favorites/FavoritesClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Heading from '@/components/Heading';
+import ListingCard from '@/components/listings/ListingCard';
 import Container from '@/components/Container';
 
 interface FavoritesClientProps {
@@ -15,6 +16,17 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({ listings, currentUser
         title='Favorites'
         subtitle='List of places you have favorited!'
       />
+      <div classname='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols- gap-8'>
+        {listings.map((listing) => {
+          return (
+            <ListingCard
+              key={listing._id}
+              currentUser={currentUser}
+              data={listing}
+            />
+          );
+        })}
+      </div>
     </Container>
   );
 };
