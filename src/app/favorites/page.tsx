35e6be66c9daae81af1f878cc2f2757fdb79a9b5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-01 19:56:35 +0100

    fix: add fetchListings useCallback handler function

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -5,9 +5,19 @@ import { useSession } from 'next-auth/react';
 
 import ClientOnly from '@/components/ClientOnly';
 import EmptyState from '@/components/EmptyState';
+import { getFavorites } from '@/services/favoriteService';
 
 const ListingPage = () => {
   const [listings, setListings] = useState([]);
+
+  const fetchListings = useCallback(async () => {
+    try {
+      const { data } = await getFavorites();
+      setListings(data);
+    } catch (err: any) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <ClientOnly>
