f5e8a7da48f5731414fb85373669f1db50ed61cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-01 15:51:35 +0100

    fix: modify

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
+
+import ClientOnly from '@/components/ClientOnly';
+import EmptyState from '@/components/EmptyState';
+
 const ListingPage = () => {
   return (
-    <div>
-      ListingPage
-    </div>
+    <ClientOnly>
+      <EmptyState
+        title='No favorites found'
+        subtitle='Looks like you have no favorite listings.'
+      />
+    </ClientOnly>
   );
 };
 
