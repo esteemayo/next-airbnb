576bb81f53e3ad0cbac0c65cf28a5e9fd2798baf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 00:48:51 +0100

    fix: modify

--- src/app/properties/PropertiesClient.tsx
+++ src/app/properties/PropertiesClient.tsx
@@ -41,7 +41,7 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
   return (
     <Container>
       <Heading
-        title='Trips'
+        title='Properties'
         subtitle={`Where you've been and where you're going`}
       />
       <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
