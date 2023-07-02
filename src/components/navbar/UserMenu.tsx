95664ad04aad6b79a553f73a766c4ec3f4b37f92
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-02 01:00:48 +0100

    fix: modify

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -71,7 +71,10 @@ const UserMenu = () => {
                   onClick={() => router.push('/reservations')}
                   label='My reservations'
                 />
-                <MenuItem onClick={() => {}} label='My properties' />
+                <MenuItem
+                  onClick={() => router.push('/properties')}
+                  label='My properties'
+                />
                 <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
                 <hr />
                 <MenuItem onClick={() => signOut()} label='Logout' />
