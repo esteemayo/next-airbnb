8c5f54772d0509cd6bfbfdf15392faa59f540f02
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-02 01:05:13 +0100

    fix: modify

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -2,16 +2,13 @@ import prisma from '@/libs/prismadb';
 
 export default async function getListings() {
   try {
-    await connectDB();
-    const listings = await Listing.find().sort('-createdAt');
-
     // const listings = await prisma.listing.findMany({
     //   orderBy: {
     //     createdAt: 'desc',
     //   },
     // });
 
-    const safeListings = listings.map((listing)=> ({
+    const safeListings = listings.map((listing) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
     }));
