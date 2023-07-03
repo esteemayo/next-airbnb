54dd3c3e6da519b44fb6949640bb96d8b50d17ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 03:44:05 +0100

    fix: add useSearchParams next/navigation hook

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import Modal from './Modal';
 import useSearchModal from '@/hooks/useSearchModal';
