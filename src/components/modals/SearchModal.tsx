aad09ba370bbda217f0f702925dfe81445357630
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 04:17:45 +0100

    fix: add useCallback react hook

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import { Range } from 'react-date-range';
