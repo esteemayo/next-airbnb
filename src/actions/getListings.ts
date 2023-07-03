d6bf114299f4054d6c7c7498ce8f17926dcdafab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 18:38:19 +0100

    fix: modify

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -54,6 +54,25 @@ export default async function getListings(params: IListingsParams) {
       query.locationValue = locationValue;
     }
 
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
