ed778bc39e079bf0e1bd6ae2c2d8b15dfacba425
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 03:53:11 +0100

    fix: add bathroomCount state value

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -20,6 +20,7 @@ const SearchModal = () => {
   const [step, setStep] = useState(STEPS.LOCATION);
   const [guestCount, setGuestCount] = useState(1);
   const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
 
   return (
     <Modal
