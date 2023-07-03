c7ba2360da5422bcc2f34348a7cc39909682ba50
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 01:00:54 +0100

    fix: handle delete listing api functionality

--- src/app/api/listings/[listingId]/route.ts
+++ src/app/api/listings/[listingId]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 import connectDB from '@/utils/db';
+import { getLoggedInUser } from '@/actions/getLoggedInUser';
 import Listing from '@/models/Listing';
 
 interface IParams {
@@ -21,6 +22,47 @@ export const GET = async (
     }
 
     const listing = await Listing.findById(listingId);
+
+    return NextResponse.json(listing, {
+      status: 200,
+    });
+  } catch (err: any) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: IParams }
+) => {
+  try {
+    await connectDB();
+
+    const currentUser = await getLoggedInUser();
+
+    if (!currentUser) {
+      return NextResponse.error();
+    }
+
+    const { listingId } = params;
+
+    if (!listingId || typeof listingId !== 'string') {
+      throw new Error('Invalid ID');
+    }
+
+    const listing = await Listing.findById(listingId);
+
+    if (!listing) {
+      return NextResponse.json('No listing found with the given ID', {
+        status: 404,
+      });
+    }
+
+    if (listing.user === String(currentUser.name)) {
+      await listing.remove();
+    }
 
     return NextResponse.json(listing, {
       status: 200,
