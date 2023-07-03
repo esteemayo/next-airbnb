7c0f444213dde202aabd501547e53c04c8c82eb7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-03 03:48:09 +0100

    fix: add step state value

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -6,11 +6,18 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import Modal from './Modal';
 import useSearchModal from '@/hooks/useSearchModal';
 
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
+
 const SearchModal = () => {
   const router = useRouter();
   const params = useSearchParams();
-
   const searchModal = useSearchModal();
+
+  const [step, setStep] = useState(STEPS.LOCATION);
 
   return (
     <Modal
