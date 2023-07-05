5e7580b71e7bcf9f005efc602d9761ba89106c22
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-05 18:22:01 +0100

    fix: fix a bug

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -58,7 +58,7 @@ const Search = () => {
       className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'
     >
       <div className='flex flex-row items-center justify-between'>
-        <div className='text-sm font-semibold px-6'>{locationValue}</div>
+        <div className='text-sm font-semibold px-6'>{locationLabel}</div>
         <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
           {durationLabel}
         </div>
