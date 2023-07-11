ce46d644e9d9078a9ea296b8120a39385de97a93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-11 02:23:35 +0100

    fix: modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
 import RentModal from '@/components/modals/RentModal';
 import SearchModal from '@/components/modals/SearchModal';
+import Navbar from '@/components/navbar/Navbar';
 
 import ToasterProvider from '@/providers/ToasterProvider';
 import AuthProvider from '@/providers/AuthProvider';
